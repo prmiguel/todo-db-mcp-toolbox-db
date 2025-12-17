f9bb7db367af0ce539377843489094c64363b0e4
prmiguel <miangel-pr@outlook.com>
2025-12-16 22:37:22 -0400

    base config for fb mcp

--- db/init.sql
+++ db/init.sql
@@ -6,18 +6,18 @@ CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
 
 START TRANSACTION;
 
-CREATE TABLE "Todos" (
-    "Id" uuid NOT NULL,
-    "Completed" boolean NOT NULL,
-    "Order" integer NOT NULL,
-    "Title" text,
-    CONSTRAINT "PK_Todos" PRIMARY KEY ("Id")
+CREATE TABLE "todos" (
+    "id" uuid NOT NULL,
+    "completed" boolean NOT NULL,
+    "order" integer NOT NULL,
+    "title" text,
+    CONSTRAINT "PK_Todos" PRIMARY KEY ("id")
 );
 
 INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
 VALUES ('20250406054858_InitialCreate', '8.0.11');
 
-INSERT INTO "Todos" ("Id", "Completed", "Order", "Title") VALUES
+INSERT INTO "todos" ("id", "completed", "order", "title") VALUES
 ('ec335d46-d575-47f9-a1f1-3e92e8aa616a',	'0',	0,	'Buy apples'),
 ('bd890bdf-0f84-4655-9f7b-f396ddc8748c', '1', 130, 'Repair bike'),
 ('51fce3c4-be96-42a0-ba93-b1893347b03f', '1', 389, 'Cook dinner'),
